041cea5ac388b58b5f61445e1ea1014d98a25b55
Gustavo <guss03112002@gmail.com>
2024-03-14 10:30:11 -0500

    add question types

--- form.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById("form_id");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log(form.elements[0].value);
-  console.log(form.elements[1].value);
-});
