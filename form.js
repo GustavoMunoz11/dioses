9da2fd195fa1156e114811933225c9fd1dc2cd84
Gustavo <guss03112002@gmail.com>
2024-03-21 10:40:24 -0500

    question 5

--- form.js
+++ form.js
@@ -7,9 +7,7 @@ BackButton.addEventListener("click", handleClickBack);
 
 let position = 0;
 function handleClickForward() {
-  if (position == 3000) {
-    position = position;
-  } else {
+  if (position < 3000) {
     position = position + 600;
     console.log(position);
     slider.style.transform = `translateX(-${position}px)`;
