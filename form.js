a49d2fbee69c188ae892514b9711c24f1257bb46
Gustavo <guss03112002@gmail.com>
2024-04-04 10:33:36 -0500

    values

--- form.js
+++ form.js
@@ -4,6 +4,7 @@ document.getElementById("slider").addEventListener("submit", function (event) {
   let user = document.getElementById("Q1").value;
   console.log(user);
   let valores = document.querySelector('input[name="p2"]:checked').value;
+  valores = Question2;
   console.log(valores);
   let frases = Array.from(
     document.querySelectorAll('input[name="p3"]:checked')
@@ -16,3 +17,23 @@ document.getElementById("slider").addEventListener("submit", function (event) {
   let descripcion = document.querySelector('input[name="p6"]:checked').value;
   console.log(descripcion);
 });
+
+let puntos = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let dioses = [
+  "Zeus",
+  "Afrodita",
+  "Anubis",
+  "Budda",
+  "Baldur",
+  "Loki",
+  "Sekhmet",
+  "SungWukong",
+];
+
+function Question2() {
+  if (valores == 1) {
+    puntos.map((e) => e - 2, 3, 4, 5, 7);
+    return [[puntos]];
+  }
+}
